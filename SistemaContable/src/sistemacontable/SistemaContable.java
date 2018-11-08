18d3b64a00e6fe1bc557c4c500858fd51c286949
Luis Cerna <luizzroberth@gmail.com>
2018-11-07 23:16:44 -0600

    Iniciando Proyecto de Sistema Contable

---
+++ SistemaContable/src/sistemacontable/SistemaContable.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemacontable;
+
+/**
+ *
+ * @author Luis Cerna
+ */
+public class SistemaContable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String variable = new String("Hola Mundo!");
+        System.out.println(variable);
+    }
+    
+}
