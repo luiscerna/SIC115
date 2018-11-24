7f91472cb4ca5dc4d4f27a27f95edf3736972d20
Robert <chesterven7@gmail.com>
2018-11-24 00:38:59 -0600

    Constructores insert y consultar

--- SistemaContable/src/Modelo/Movimiento.java
+++ SistemaContable/src/Modelo/Movimiento.java
@@ -1,6 +1,10 @@
 package Modelo;
 
+import Datos.Conexion;
+import java.sql.SQLException;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Movimiento {
     // Atributos
@@ -10,10 +14,35 @@ public class Movimiento {
     private int cant;
     private double monto;
     private double precioU;
+    private int codMaterial;
 
     // Constructor
     public Movimiento() {
         
+    }
+    
+    //Constructor de consulta
+    public Movimiento(int idMov){
+     try {
+            Conexion conexion = new Conexion();
+            this.setIdMov(idMov);
+            String query;
+            query = "SELECT * FROM Movimiento WHERE idMov= "+ this.getIdMov()+";";
+            conexion.pst = conexion.conectar().prepareStatement(query);
+            conexion.rs = conexion.pst.executeQuery();
+            if(conexion.rs.next()){
+                this.setIdMov(conexion.rs.getInt("idMov"));
+                this.setCodMaterial(conexion.rs.getInt("codMaterial"));
+                this.setFechaMov(conexion.rs.getDate("fechaMov"));
+                this.setEsSalida(conexion.rs.getBoolean("esSalida"));
+                this.setCant(conexion.rs.getInt("cant"));
+                this.setMonto(conexion.rs.getDouble("montoT"));
+                this.setPrecioU(conexion.rs.getDouble("precioU"));   
+            }
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(DetalleTransaccion.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     // Métodos getter y setter
@@ -63,6 +92,14 @@ public class Movimiento {
 
     public void setPrecioU(double precioU) {
         this.precioU = precioU;
+    }
+
+    public int getCodMaterial() {
+        return codMaterial;
+    }
+
+    public void setCodMaterial(int codMaterial) {
+        this.codMaterial = codMaterial;
     }
     
 }
--- SistemaContable/src/Modelo/Usuario.java
+++ SistemaContable/src/Modelo/Usuario.java
@@ -1,7 +1,11 @@
 package Modelo;
 
 
+import Datos.Conexion;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Usuario extends Empleado {
     // Atributos
@@ -17,12 +21,54 @@ public class Usuario extends Empleado {
     }
     //Constructor de Consulta
     public Usuario(int idUsuario) {
-        
+         try {
+            Conexion conexion = new Conexion();
+            this.setIdUsuario(idUsuario);
+            String query;
+            query = "SELECT * FROM Usuario WHERE idUsuario = "+ this.getIdUsuario()+";";
+            conexion.pst = conexion.conectar().prepareStatement(query);
+            conexion.rs = conexion.pst.executeQuery();
+            if(conexion.rs.next()){
+                this.setIdUsuario(conexion.rs.getInt("idUsuario"));
+                this.setUsuario(conexion.rs.getString("usuario"));
+                this.setPassword(conexion.rs.getString("password"));
+                this.setNivelAcceso(conexion.rs.getInt("nivelAcceso"));
+            }
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(DetalleTransaccion.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     //Constructor de Nuevo Registro
-    public Usuario(String usuario, String password, int nivel){
-        
+    public Usuario(String usuario, String password, int nivel, String codEmpleado, String nombre, String apellido, int puesto){ //Agregue otros parametos necesarios para el insert
+         try {
+             Conexion conexion = new Conexion();
+            String query;
+                    query = "SELECT idUsuario FROM Usuario ORDER BY idUsuario DESC LIMIT 1;";
+                    conexion.pst= conexion.conectar().prepareStatement(query);
+                    conexion.rs = conexion.pst.executeQuery();
+                    conexion.rs.next();
+                    int id = conexion.rs.getInt("idUsuario");
+                    id += 1;
+                    this.setIdUsuario(id); 
+             
+            query = "INSERT INTO Usuario (codEmpleado, idUsuario, codPuesto, nombreEmpleado, apellidoEmpleado, usuario, password, nivelAcceso) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            conexion.pst= conexion.conectar().prepareStatement(query);
+            conexion.pst.setString(1, codEmpleado);
+            conexion.pst.setInt(2, getIdUsuario());
+            conexion.pst.setInt(3, puesto);
+            conexion.pst.setString(4, nombre);
+            conexion.pst.setString(5, apellido);
+            conexion.pst.setString(6, usuario);
+            conexion.pst.setString(7, password);
+            conexion.pst.setInt(7, nivel);
+            conexion.pst.executeUpdate();
+            System.out.println("Se ha registrado exitosamente en usuarios.");
+             } catch (SQLException ex) {
+            Logger.getLogger(DetalleTransaccion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
     }
     
     // Métodos getter y setter
