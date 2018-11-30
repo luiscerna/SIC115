f571a2b8a4374e8107e03ddbeeee0fc5b9aba491
Luis Cerna <luizzroberth@gmail.com>
2018-11-29 22:39:43 -0600

    Creando clase EmpleadoControl en paquete Controlador

---
+++ SistemaContable/src/Controlador/EmpleadoControl.java
@@ -0,0 +1,27 @@
+package Controlador;
+
+
+public class EmpleadoControl {
+    
+    // Constructor
+    public EmpleadoControl(){
+        
+    }
+    
+    /*
+        METODOS DE LUIS
+    */
+    
+    
+    
+    
+    
+
+    /*
+        METODOS DE IRENE
+    */
+    
+    
+    
+    
+}
